a5eee612d95561b7c18caf81f00b0bf853278324
hacha <hachatryan.artem.2002@gmail.com>
2023-09-09 20:41:08 +0300

    Added html code to Hackathone Description page

---
+++ front/src/pages/HackatonDescriptionPage.jsx
@@ -0,0 +1,24 @@
+
+<>
+    <section className="flex flex-col justify-center antialiased bg-gray-900 text-gray-200 min-h-screen">
+        <div className="max-w-6xl mx-auto p-4 sm:px-6 h-full mb-5">
+            <article className="max-w-sm mx-auto md:max-w-none grid md:grid-cols-2 gap-6 md:gap-8 lg:gap-12 xl:gap-16 items-center">
+                <a className="relative block group" href="#0">
+                    <div className="absolute inset-0 bg-gray-800 hidden md:block transform md:translate-y-2 md:translate-x-4 xl:translate-y-4 xl:translate-x-8 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out pointer-events-none" aria-hidden="true"></div>
+                    <figure className="relative h-0 pb-[56.25%] md:pb-[75%] lg:pb-[56.25%] overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out">
+                        <img className="absolute inset-0 w-full h-full object-cover transform hover:scale-105 transition duration-700 ease-out" src="https://preview.cruip.com/open-pro/images/blog-post-01.jpg" width="540" height="303" alt="Blog post"/>
+                    </figure>
+                </a>
+                <div>
+                    <header>
+                        <h3 className="text-2xl lg:text-3xl font-bold leading-tight mb-2">
+                            <a className="hover:text-gray-100 transition duration-150 ease-in-out" href="#0">Hacaton name</a>
+                        </h3>
+                    </header>
+                    <p className="text-lg text-gray-400 flex-grow">Hackaton full description Hackaton full description Hackaton full description Hackaton full description Hackaton full description Hackaton full description Hackaton full description Hackaton full description Hackaton full description Hackaton full description</p>
+                    <p className="text-lg text-white-400 flex-grow">19.01.2023-23.01.2023</p>
+                </div>
+            </article>
+        </div>
+    </section>
+</>
