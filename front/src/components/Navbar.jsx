c08e61f0106d0446fd53d4862963dde06ed15e3f
Starman-in-the-sky <ms.practice@yandex.ru>
2023-09-09 18:11:09 +0300

    ParticipantsTable and minor changes

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ export const Navbar = () => {
                     </div>
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div className="flex flex-shrink-0 items-center">
-                            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"/>
+                            <img className="h-8 w-auto" src="../assets/images/itam32.jpg" alt="Your Company"/>
                         </div>
                         <div className="hidden sm:ml-6 sm:block">
                             <div className="flex space-x-4">
