53d0622d0df61e0b133cdce6f69f3a1d5c0220fa
DeniRadionov <108937144+DeniRadionov@users.noreply.github.com>
2023-09-10 18:54:32 +0300

    TG1

--- tg-bot/main.py
+++ tg-bot/main.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 import requests
 import re
 ###
-url = 'http://localhost:5080/register'
+
 
 
 
@@ -27,6 +27,8 @@ def start(message):
 @bot.message_handler(commands=['register'])
 def reg(message):
     global data_r
+    global url
+    url = 'http://localhost:5080/register'
     data_r = {
         "fullName": "",
         "telegram": "",
@@ -81,6 +83,110 @@ def reg6(message):
         mes = f"""Вы уже зарегестрировались."""
         bot.send_message(message.chat.id, mes, parse_mode='html')
 
+@bot.message_handler(commands=['create_team'])
+def create_team(message):
+    global data_c_t
+    global url
+    url = 'http://localhost:5080/register'
+    data_c_t = {
+  "name": "",
+  "leaderUserName": "",
+  "memberUserNames": [
+  ]
+}
+    mes = f"""Название команды"""
+    bot.send_message(message.chat.id, mes, parse_mode='html')
+    bot.register_next_step_handler(message, create_team_name)
+def create_team_name(message):
+    data_c_t["name"] = message.text
+    mes = f"""Добавьте сокомадников по очереди, максимальное количество в команде 5 человек. Если число сокомадников меньше 5, то напиши /stop в конце.
+Пример:
+\t@Name1
+\t@Name2
+\t/stop"""
+    data_c_t["leaderUserName"] = message.from_user.username
+    bot.send_message(message.chat.id, mes, parse_mode='html')
+    bot.register_next_step_handler(message, create_team_member1)
+
+
+def create_team_member1(message):
+    if message.text == '/stop':
+        response = requests.post(url, json=data_c_t)
+
+        # Проверяем, был ли запрос успешным (код ответа 200)
+        if response.status_code == 200:
+            mes = f"""Команда создана! 1 участник из 5."""
+            bot.send_message(message.chat.id, mes, parse_mode='html')
+            return
+        else:
+            mes = f"""Такая команда уже есть."""
+            bot.send_message(message.chat.id, mes, parse_mode='html')
+            return
+    data_c_t["memberUserNames"].append(message.from_user.username)
+    mes = 'Вы добавили первого сокомадника.'
+    bot.send_message(message.chat.id, mes, parse_mode='html')
+
+    bot.register_next_step_handler(message, create_team_member2)
+def create_team_member2(message):
+    if message.text == '/stop':
+        response = requests.post(url, json=data_c_t)
+
+        # Проверяем, был ли запрос успешным (код ответа 200)
+        if response.status_code == 200:
+            mes = f"""Команда создана! 2 участник из 5."""
+            bot.send_message(message.chat.id, mes, parse_mode='html')
+            return
+        else:
+            mes = f"""Такая команда уже есть."""
+            bot.send_message(message.chat.id, mes, parse_mode='html')
+            return
+    data_c_t["memberUserNames"].append(message.from_user.username)
+    mes = 'Вы добавили второго сокомадника.'
+    bot.send_message(message.chat.id, mes, parse_mode='html')
+
+    bot.register_next_step_handler(message, create_team_member3)
+def create_team_member3(message):
+    if message.text == '/stop':
+        response = requests.post(url, json=data_c_t)
+
+        # Проверяем, был ли запрос успешным (код ответа 200)
+        if response.status_code == 200:
+            mes = f"""Команда создана! 3 участник из 5."""
+            bot.send_message(message.chat.id, mes, parse_mode='html')
+            return
+        else:
+            mes = f"""Такая команда уже есть."""
+            bot.send_message(message.chat.id, mes, parse_mode='html')
+            return
+    data_c_t["memberUserNames"].append(message.from_user.username)
+    mes = 'Вы добавили третьего сокомадника.'
+    bot.send_message(message.chat.id, mes, parse_mode='html')
+
+    bot.register_next_step_handler(message, create_team_member4)
+
+def create_team_member4(message):
+    if message.text == '/stop':
+        response = requests.post(url, json=data_c_t)
+
+        # Проверяем, был ли запрос успешным (код ответа 200)
+        if response.status_code == 200:
+            mes = f"""Команда создана! 4 участник из 5."""
+            bot.send_message(message.chat.id, mes, parse_mode='html')
+            return
+        else:
+            mes = f"""Такая команда уже есть."""
+            bot.send_message(message.chat.id, mes, parse_mode='html')
+            return
+    data_c_t["memberUserNames"].append(message.from_user.username)
+    response = requests.post(url, json=data_c_t)
+    if response.status_code == 200:
+        mes = 'Вы добавили четвертого сокомадника. В команде теперь 5 участников.'
+        bot.send_message(message.chat.id, mes, parse_mode='html')
+        return
+    else:
+        mes = f"""Такая команда уже есть."""
+        bot.send_message(message.chat.id, mes, parse_mode='html')
+        return
 
 bot.polling(none_stop=True)
 
