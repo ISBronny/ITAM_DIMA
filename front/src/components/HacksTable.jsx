f94422438cc4ae05a0857f951d7f0c327662372f
hacha <hachatryan.artem.2002@gmail.com>
2023-09-11 00:50:02 +0300

    Added fonts

--- front/src/components/HacksTable.jsx
+++ front/src/components/HacksTable.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import {NavLink} from "react-router-dom";
-
+import "../styles/main.css"
 
 export const HacksTable = ({user, search= false}) => {
 
@@ -26,6 +26,11 @@ export const HacksTable = ({user, search= false}) => {
 
     return (
         <>
+            <>
+                <style>
+                    @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&family=M+PLUS+Rounded+1c:wght@700&family=Overpass:wght@500&display=swap');
+                </style>
+            </>
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                 {search ?                 <div className="pb-4 bg-white dark:bg-gray-900">
                         <label htmlFor="table-search"
@@ -46,7 +51,7 @@ export const HacksTable = ({user, search= false}) => {
                             </div>
                             <input type="text"
                                    id="table-search"
-                                   className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                   className="comforta block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                    placeholder="Поиск команды"
                                    onChange={event => setState({...state, filter: event.currentTarget.value})}
                             />
@@ -54,7 +59,7 @@ export const HacksTable = ({user, search= false}) => {
                     </div>
                     : ""}
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <thead className="comforta text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col"
                             className="p-4">
@@ -80,7 +85,7 @@ export const HacksTable = ({user, search= false}) => {
                     </thead>
                     <tbody>
                     {state.isLoading ? <tr></tr> : state.hacks.map(h =>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <tr className="comforta bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                             <th scope="row"
                                 className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {h.name}
