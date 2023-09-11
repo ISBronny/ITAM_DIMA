d693c90b9e96f2b73e0392e4a97e8e2e6626546a
Starman-in-the-sky <ms.practice@yandex.ru>
2023-09-11 12:59:59 +0300

    вв

--- front/src/components/ParticipantsTable.jsx
+++ front/src/components/ParticipantsTable.jsx
@@ -54,7 +54,7 @@ export const ParticipantsTable = ({search= true, teamId = undefined}) => {
                             <input type="text"
                                    id="table-search"
                                    className="comforta block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                   placeholder="Поиск команды"
+                                   placeholder="Поиск участника"
                                    onChange={event => setState({...state, filter: event.currentTarget.value})}
                             />
                         </div>
