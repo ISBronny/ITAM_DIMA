9c351436cee9289b146329842ffa8b10a9916997
hacha <hachatryan.artem.2002@gmail.com>
2023-09-11 03:51:30 +0300

    Added navbar shadow

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ export const Navbar = () => {
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:ital,wght@1,300&family=Montserrat+Alternates:wght@500&family=Mulish:wght@300&display=swap');
         </style>
-        <nav className="bg-gray-800">
+        <nav className="bg-gray-800 shadow shadow-gray-700">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
